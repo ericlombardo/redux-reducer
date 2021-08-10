b72923fbc76fb12d7e58d8cac1423d131bea8663
Eric <eric.m.lombardo@gmail.com>
2021-08-10 19:05:42 -0400

    Use reducers to set state

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,23 @@
-export function manageFriends(state, action){
+export function manageFriends(state = {friends: []}, action){
+  switch (action.type) {
+    case 'ADD_FRIEND':
+      return {friends: state.friends.concat(action.friend)}
+      break
+    case 'REMOVE_FRIEND': // checked solution to see how they handled this
+      // finds index of friend we want to delete
+      const friendIndex = state.friends.findIndex(friend => friend.id === action.id) 
+      return (
+        {...state, // copies rest of state
+          friends: [ // goes into friends
+            ...state.friends.slice(0, friendIndex), // adds all friends except friend
+            ...state.friends.slice(friendIndex + 1) // we want to remove by slicing it out
+          ]
+        }
+      )
+      
+      break
+    default:
+      return state
+      break
+  }
 }
